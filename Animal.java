04a65b2229ea250bafde2879ae5874f05dc7a63c
Sloan Ireland <sireland30@stuy.edu>
2021-10-29 11:35:59 -0400

    base file created

---
+++ Animal.java
@@ -0,0 +1,19 @@
+public class Animal {
+    private String noise;
+    private int age;
+    private String name;
+
+    public Animal(String noise, int age, String name){
+        this.noise = noise;
+        this.age = age;
+        this.name = name;
+    }
+
+    public void speak(){
+      /*print the following message replacing the ? with name/age/noise:
+      My name is ?.
+      I am ? years old.
+      I say ?.
+      */
+    }
+}
