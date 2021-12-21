4ff3208b75d57f38645e617382ccde3fbfaab669
Sloan Ireland <sireland30@stuy.edu>
2021-12-21 11:47:49 -0500

    base file

---
+++ IntegerParseInt/Integer.java
@@ -0,0 +1,10 @@
+public class Integer {
+  public static int valueOfDigit(char c) {
+    //helper method to convert one char to Integer
+    
+  }
+  public static int stringToInt(String s){
+    //return the value of Integer.parseInt(s) but do it yourself!
+    //Do not use any built in parse methods.
+  }
+}
