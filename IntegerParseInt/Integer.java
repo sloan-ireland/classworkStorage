fd97d474cb8ab0b63da16281fddcb22f6a02bd98
Sloan <sireland30@stuy.edu>
2021-12-21 21:27:46 -0500

    made it work with negative numbers

--- IntegerParseInt/Integer.java
+++ IntegerParseInt/Integer.java
@@ -8,7 +8,11 @@ public class Integer {
     //return the value of Integer.parseInt(s) but do it yourself!
     //Do not use any built in parse methods.
     int result = 0;
-    for (int i = 0; i < s.length(); i++) {
+    int negativeDecider = 0;
+    if (s.charAt(0) == '-'){
+      negativeDecider++;
+    }
+    for (int i = 0 + negativeDecider; i < s.length(); i++) {
       if (i == s.length() -1) {
         result += valueOfDigit(s.charAt(i));
       }
@@ -16,11 +20,15 @@ public class Integer {
         result += valueOfDigit(s.charAt(i)) * (Math.pow(10, s.length() - 1 -i));
       }
     }
+    if (s.charAt(0) == '-'){
+      result /= -1;
+    }
     return result;
   }
 
+
   public static void main(String[] args) {
     System.out.println(valueOfDigit('2'));
-    System.out.println(stringToInt("200"));
+    System.out.println(stringToInt("-29979"));
   }
 }
