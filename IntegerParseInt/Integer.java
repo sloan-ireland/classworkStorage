486d6da6ed5c4dad8d904c79422d28af934d6658
Sloan <sireland30@stuy.edu>
2021-12-21 20:39:41 -0500

    have most of stringToInt figured out

--- IntegerParseInt/Integer.java
+++ IntegerParseInt/Integer.java
@@ -7,10 +7,20 @@ public class Integer {
   public static int stringToInt(String s){
     //return the value of Integer.parseInt(s) but do it yourself!
     //Do not use any built in parse methods.
-    return 0;
+    int result = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (i == s.length() -1) {
+        result += valueOfDigit(s.charAt(i));
+      }
+      else {
+        result += valueOfDigit(s.charAt(i)) * ((10 * (s.length() - 1 - i)));
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
     System.out.println(valueOfDigit('2'));
+    System.out.println(stringToInt("200"));
   }
 }
