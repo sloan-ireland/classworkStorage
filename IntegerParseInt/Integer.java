c8d619bee12b582f2060321fdfe601d8e752c714
Sloan <sireland30@stuy.edu>
2021-12-21 20:14:21 -0500

    valueOfChar done and tested. works given that char is positive

--- IntegerParseInt/Integer.java
+++ IntegerParseInt/Integer.java
@@ -1,10 +1,16 @@
 public class Integer {
   public static int valueOfDigit(char c) {
     //helper method to convert one char to Integer
-    
+    return (c - '0');
+
   }
   public static int stringToInt(String s){
     //return the value of Integer.parseInt(s) but do it yourself!
     //Do not use any built in parse methods.
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(valueOfDigit('2'));
   }
 }
