d224829fedb05e87464b80a559e41d84f37afcba
Sloan Ireland <sireland30@stuy.edu>
2022-01-26 11:44:57 -0500

    done with isEven

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -8,7 +8,23 @@ public class Factorial {
     }
   }
 
+  public static boolean isEven (int x) {
+    if (x < 0) {
+      return isEven(-x);
+    }
+    if (x > 1) {
+      return isEven(x-2);
+    }
+    else {
+      if (x == 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   public static void main(String[] args) {
-    System.out.println(Fact(10));
+    //System.out.println(Fact(10));
+    System.out.println(isEven(-10));
   }
 }
