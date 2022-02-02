d59f6a8e1c13c6896fb8e5782c03a41fe9c836dd
Sloan Ireland <sireland30@stuy.edu>
2022-02-02 11:18:14 -0500

    base file

---
+++ Recursion/Makewords.java
@@ -0,0 +1,12 @@
+public class Makewords {
+  public static void makeWords(int size, String ans) {
+    
+  }
+  public static void main(String[] args) {
+    makeWords(3,"");
+    for (char c = 'a'; c <= 'z'; c++) {
+      System.out.print(c);
+    }
+    System.out.println();
+  }
+}
