6aaf00fd547af5855fee35b6938bf33ced3f8866
Sloan Ireland <sireland30@stuy.edu>
2022-02-03 11:14:38 -0500

    works now

--- Recursion/Makewords.java
+++ Recursion/Makewords.java
@@ -1,18 +1,15 @@
 public class Makewords {
   public static void makeWords(int size, String ans) {
-    if (size != 0) {
+    if (size > 0) {
       for (char c = 'a'; c <= 'z'; c++) {
-        System.out.print(c);
+        makeWords(size - 1, ans + c);
       }
     }
     else {
       System.out.println(ans);
-      System.out.println(makeWords(size -1))
     }
   }
   public static void main(String[] args) {
     makeWords(3,"");
-
-    System.out.println();
   }
 }
